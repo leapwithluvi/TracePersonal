8f539e78d4dbb5b4f9d4917c03406aba1b8e2f3e
Luvi <luviaprilyansyahgabriel@gmail.com>
2025-07-22 17:19:10 +0800

    Membuat hanya yang sudah login bisa masuk page leaderboards

--- frontend/src/pages/Leaderboard.jsx
+++ frontend/src/pages/Leaderboard.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
-import Layout from './Layout'
-import Leaderboards from '../components/Leaderboards'
+import React, { useEffect } from "react";
+import Layout from "./Layout";
+import { useDispatch, useSelector } from "react-redux";
+import Leaderboards from "../components/Leaderboards";
+import { useNavigate } from "react-router-dom";
+import { getMe } from "../features/authSlice";
 
 const Leaderboard = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { isError } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    dispatch(getMe());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (isError) {
+      navigate("/");
+    }
+  }, [isError, navigate]);
   return (
     <Layout>
-        <Leaderboards />
+      <Leaderboards />
     </Layout>
-  )
-}
+  );
+};
 
-export default Leaderboard
+export default Leaderboard;
