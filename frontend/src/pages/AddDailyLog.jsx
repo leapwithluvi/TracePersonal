d010bb3603d2a12f7280372244cc571e94266c77
Luvi <luviaprilyansyahgabriel@gmail.com>
2025-07-24 17:07:10 +0800

    menambahkan file create daily dan edit daily

---
+++ frontend/src/pages/AddDailyLog.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "./Layout";
+import CreateDailyLog from "../components/CreateDailyLog";
+
+const AddDailyLog = () => {
+  return (
+    <Layout>
+      <CreateDailyLog />
+    </Layout>
+  );
+};
+
+export default AddDailyLog;
