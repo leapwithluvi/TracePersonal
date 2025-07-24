9219f4b1b0d616cd5adcacd1112d3132b166333d
Luvi <luviaprilyansyahgabriel@gmail.com>
2025-07-24 17:08:22 +0800

    menambahkan isi page (lupa)

---
+++ frontend/src/pages/EditDailyLog.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "./Layout";
+import EditDailyLog from "../components/EditDailyLog";
+
+const EditDailyLog = () => {
+  return (
+    <Layout>
+      <EditDailyLog />
+    </Layout>
+  );
+};
+
+export default EditDailyLog;
