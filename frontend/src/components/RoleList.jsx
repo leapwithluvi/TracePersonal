ea88e7abff6b69b7088b100efde0902ee805fcfb
Luvi <luviaprilyansyahgabriel@gmail.com>
2025-07-29 15:58:04 +0800

    Mengubah tampilan roles dan menambahkan pagination dan search

--- frontend/src/components/RoleList.jsx
+++ frontend/src/components/RoleList.jsx
@@ -4,6 +4,31 @@ import axios from "axios";
 
 const RoleList = () => {
   const [roles, setRoles] = useState([]);
+  const [filteredRoles, setFilteredRoles] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  // Pagination
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10;
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = filteredRoles.slice(indexOfFirstItem, indexOfLastItem);
+  const totalPages = Math.ceil(roles.length / itemsPerPage);
+
+  // search
+  useEffect(() => {
+    const keyword = searchQuery.trim().toLowerCase();
+
+    if (keyword === "") {
+      setFilteredRoles(roles);
+    } else {
+      const filtered = roles.filter((user) =>
+        user.name.toLowerCase().includes(keyword)
+      );
+      setFilteredRoles(filtered);
+    }
+  }, [searchQuery, roles]);
+  // end search
 
   useEffect(() => {
     getRoles();
@@ -15,18 +40,23 @@ const RoleList = () => {
   };
 
   const deleteRole = async (roleId) => {
-        const confirm = window.confirm(
-          "Apakah Anda yakin ingin menghapus data ini?"
-        );
-        if (!confirm) return;
-        
+    const confirm = window.confirm(
+      "Apakah Anda yakin ingin menghapus data ini?"
+    );
+    if (!confirm) return;
+
     await axios.delete(`http://localhost:5000/roles/${roleId}`);
-    getRoles(); 
+    getRoles();
   };
   return (
     <div>
-      <h1 className="text-4xl font-semibold text-white">Roles</h1>
-      <div className="mt-4">
+      <h1 className="text-4xl font-semibold text-white">CRUD Roles</h1>
+      <p className="text-white">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos
+        culpa veritatis <br /> accusantium sint labore, quisquam natus dolorum.
+        Delectus, quidem totam?
+      </p>
+      <div className="mt-6">
         <Link
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
           to="/roles/add"
@@ -34,84 +64,116 @@ const RoleList = () => {
           Create Role
         </Link>
       </div>
-      <div className="mt-4 p-6 dark:bg-gray-800 rounded-lg shadow-md">
-        <p className="text-2xl text-white mb-4">List Data Roles: </p>
-        <div class="flex flex-col">
-          <div class=" overflow-x-auto pb-4">
-            <div class="min-w-full inline-block align-middle">
-              <div class="overflow-hidden  border rounded-lg border-gray-300">
-                <table class="table-auto min-w-full rounded-xl">
-                  <thead>
-                    <tr class="dark:bg-gray-800">
-                      <th class=""></th>
-                      <th
-                        scope="col"
-                        class="p-5 text-left whitespace-nowrap text-sm leading-6 font-semibold text-white capitalize"
-                      >
-                        {" "}
-                        ID{" "}
-                      </th>
-                      <th
-                        scope="col"
-                        class="p-5 text-left whitespace-nowrap text-sm leading-6 font-semibold text-white capitalize min-w-[150px]"
-                      >
-                        {" "}
-                        Name{" "}
-                      </th>
-                      <th
-                        scope="col"
-                        class="p-5 text-left whitespace-nowrap text-sm leading-6 font-semibold text-white capitalize"
-                      >
-                        {" "}
-                        Join Date{" "}
-                      </th>
-                      <th
-                        scope="col"
-                        class="p-5 text-left whitespace-nowrap text-sm leading-6 font-semibold text-white capitalize"
-                      >
-                        {" "}
-                        Created By{" "}
-                      </th>
-                      <th
-                        scope="col"
-                        class="p-5 text-left whitespace-nowrap text-sm leading-6 font-semibold text-white capitalize"
-                      >
-                        {" "}
-                        Actions{" "}
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody class="divide-y divide-gray-300 ">
-                    {roles?.map((role, index) => (
-                      <tr
-                        class="dark:bg-gray-800 transition-all duration-500 hover:bg-gray-700 shadow-2xl rounded-xl transform transition-all duration-300 hover:scale-[1.01] animate-fade-in"
-                        // key={role.id}
-                      >
-                        <td class=""></td>
-                        <td class="p-5 whitespace-nowrap text-sm leading-6 font-medium text-white">
-                          {index + 1}
-                        </td>
-                        <td class=" px-5 py-3">
-                          <div class="w-48 flex items-center gap-3">
-                            <div class="data">
-                              <p class="font-normal text-sm text-white">
-                                {role.name}
-                              </p>
-                            </div>
+      <div class="mt-12 md:flex md:items-center md:justify-between">
+        <div class="relative flex items-center mt-4 md:mt-0">
+          <span class="absolute">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-5 h-5 mx-3 text-gray-400 dark:text-gray-600"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+              />
+            </svg>
+          </span>
+
+          <input
+            type="text"
+            placeholder="Search roles..."
+            onChange={(e) => setSearchQuery(e.target.value)}
+            class="block w-full py-1.5 pr-5 text-gray-700 bg-white border border-gray-200 rounded-lg md:w-80 placeholder-gray-400/70 pl-11 rtl:pr-11 rtl:pl-5 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+          />
+        </div>
+      </div>
+
+      <div class="flex flex-col mt-6">
+        <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+            <div class="overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg">
+              <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+                <thead class="bg-gray-50 dark:bg-gray-800">
+                  <tr>
+                    <th
+                      scope="col"
+                      class="py-3.5 px-4 text-sm font-normal text-center rtl:text-right text-gray-500 dark:text-gray-400"
+                    >
+                      <span>#</span>
+                    </th>
+                    <th
+                      scope="col"
+                      class="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
+                    >
+                      <span>Name</span>
+                    </th>
+                    <th
+                      scope="col"
+                      class="px-12 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
+                    >
+                      Join Date
+                    </th>
+
+                    <th
+                      scope="col"
+                      class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
+                    >
+                      Created By
+                    </th>
+
+                    <th
+                      scope="col"
+                      class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
+                    >
+                      Actions
+                    </th>
+
+                    <th scope="col" class="relative py-3.5 px-4">
+                      <span class="sr-only">Edit</span>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody class="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
+                  {filteredRoles.length === 0 ? (
+                    <p className="text-gray-600 dark:text-gray-300">
+                      Tidak ada role ditemukan.
+                    </p>
+                  ) : (
+                    currentItems.map((roles, index) => (
+                      <tr key={roles.id}>
+                        <td class="px-12 py-4 text-sm font-medium whitespace-nowrap">
+                          <div class="font-medium text-gray-800 dark:text-white text-center">
+                            {index + 1 + (currentPage - 1) * itemsPerPage}
                           </div>
                         </td>
-                        <td class="p-5 whitespace-nowrap text-sm leading-6 font-medium text-white">
-                          {role.createdAt}
+                        <td class="px-4 py-4 text-sm font-medium whitespace-nowrap">
+                          <div>
+                            <h2 class="font-medium text-gray-800 dark:text-white ">
+                              {roles.name}
+                            </h2>
+                          </div>
                         </td>
-                        <td class="p-5 whitespace-nowrap text-sm leading-6 font-medium text-white">
-                          <p class="font-normal text-sm text-white">
-                            {role.user?.name ?? "Unknown"}
-                          </p>
+                        <td class="px-12 py-4 text-sm font-medium whitespace-nowrap">
+                          <div class="inline px-3 py-1 text-sm font-normal rounded-full text-white dark:bg-gray-800">
+                            {roles.createdAt.slice(0, 10)}
+                          </div>
                         </td>
+                        <td class="px-4 py-4 text-sm whitespace-nowrap">
+                          <div>
+                            <h4 class="text-gray-700 dark:text-gray-200 inline px-3 py-1 text-sm font-normal rounded-full text-white dark:bg-gray-800">
+                              {roles.user?.name ?? "Unknown"}
+                            </h4>
+                          </div>
+                        </td>
+
                         <td class="flex p-5 items-center gap-1.5">
                           <Link
                             class="p-2  rounded-full bg-white group transition-all duration-500 hover:bg-indigo-600 flex item-center"
-                            to={`/roles/edit/${role.id}`}
+                            to={`/roles/edit/${roles.id}`}
                           >
                             <svg
                               class="cursor-pointer"
@@ -130,7 +192,7 @@ const RoleList = () => {
                           </Link>
                           <button
                             class="p-2 rounded-full bg-white group transition-all duration-500 hover:bg-red-600 flex item-center"
-                            onClick={() => deleteRole(role.id) }
+                            onClick={() => deleteRole(roles.id)}
                           >
                             <svg
                               class=""
@@ -149,12 +211,70 @@ const RoleList = () => {
                           </button>
                         </td>
                       </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
+                    ))
+                  )}
+                </tbody>
+              </table>
             </div>
           </div>
+        </div>
+      </div>
+      <div class="mt-6 sm:flex sm:items-center sm:justify-between ">
+        <div class="text-sm text-gray-500 dark:text-gray-400">
+          Page{" "}
+          <span className="font-medium text-gray-700 dark:text-gray-100">
+            {currentPage} of {totalPages}
+          </span>
+        </div>
+
+        <div class="flex items-center mt-4 gap-x-4 sm:mt-0">
+          <button
+            onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
+            disabled={currentPage === 1}
+            class="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md sm:w-auto gap-x-2 hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-5 h-5 rtl:-scale-x-100"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"
+              />
+            </svg>
+
+            <span>previous</span>
+          </button>
+
+          <button
+            onClick={() =>
+              setCurrentPage((prev) => Math.min(prev + 1, totalPages))
+            }
+            disabled={currentPage === 10}
+            class="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md sm:w-auto gap-x-2 hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800"
+          >
+            <span>Next</span>
+
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-5 h-5 rtl:-scale-x-100"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+              />
+            </svg>
+          </button>
         </div>
       </div>
     </div>
